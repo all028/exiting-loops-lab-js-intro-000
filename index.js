cf926bbac4815320e5b7c34b0aff8c8a094f7f17
al <albliu@email.com>
2019-11-28 23:04:29 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,15 @@
+function breakOut(array, changeValue, stopValue){
+
+  for (let x=0; x < stopValue; x++){
+    array[x] = changeValue;
+  }//end for
+  return array;
+}
+
+function keepGoing(array, changeValue, skipValue){
+
+}
+
+function findBy(array, findFn){
+
+}
