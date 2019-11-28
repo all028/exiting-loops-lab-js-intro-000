36cfb96720dc1b9307aa3fd06785f382cc72c9aa
al <albliu@email.com>
2019-11-28 23:47:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function breakOut(array, changeValue, stopValue){
 function keepGoing(array, changeValue, skipValue){
 
   for (let i=0; i < array.length; i++){
-    if (changeValue === skipValue)
+    if (changeValue == skipValue)
       continue
     else
       array[i] = changeValue;
